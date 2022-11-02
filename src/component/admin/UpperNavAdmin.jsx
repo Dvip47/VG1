8fa73c949e6ebe33098f5179ab1f8800d93ffeb5
Wasif Abbas <WasifAbbas900@gmail.com>
2022-11-02 19:04:22 +0530

    new change

--- src/component/admin/UpperNavAdmin.jsx
+++ src/component/admin/UpperNavAdmin.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-// import WhatsAppIcon from "@mui/icons-material/WhatsApp";
-// import FacebookIcon from "@mui/icons-material/Facebook";
-// import InstagramIcon from "@mui/icons-material/Instagram";
-// import FmdGoodIcon from "@mui/icons-material/FmdGood";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FmdGoodIcon from "@mui/icons-material/FmdGood";
 import { NavLink } from "react-router-dom";
 function UpperNavAdmin() {
   return (
@@ -45,7 +45,7 @@ function UpperNavAdmin() {
           >
             Log Out
           </NavLink>
-          {/* <i>
+          <i>
             <a href="">
               <FacebookIcon
                 style={{
@@ -64,7 +64,7 @@ function UpperNavAdmin() {
             <a href="">
               <WhatsAppIcon />
             </a>
-          </i> */}
+          </i>
         </div>
       </div>
     </>
